15d4378c8da966701f58cdf60a8c34c995b0cf8d
kanade <3631214@connect.hku.hk>
2025-04-12 00:04:43 +0800

    Create main file

---
+++ main.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+import pandas as pd
+
+if __name__ == "__main__":
+    wn = tk.Tk()
+    wn.title("Computational Finance")
+    wn.geometry("800x600")
+    wn.configure(bg="white")
+    wn.mainloop()
