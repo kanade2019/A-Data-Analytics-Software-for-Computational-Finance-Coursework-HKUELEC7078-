a89a638481794a1e1ee3663073dddc69ca92f750
kanade <3631214@connect.hku.hk>
2025-04-12 18:59:02 +0800

    主要框架

---
+++ draw_figure.py
@@ -0,0 +1,55 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import tkinter as tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+
+class DrawFigure:
+    def __init__(self, frame_figure):
+        self.frame_figure = frame_figure
+        self.fig = None
+        self.start_index = None
+        self.end_index = None
+        self.data = None
+
+    def load_data_csv(self, company_name):
+        """Load data into the figure."""
+        """Date","Price","Open","High","Low","Vol.","Change %"""
+        self.data = pd.read_csv(company_name + " Stock Price History.csv")
+        self.start_index = 0
+        self.end_index = len(self.data)
+
+    def Draw(self):
+        """Draw the figure."""
+        if self.fig is not None:
+            plt.close(self.fig)
+
+        self.fig = plt.Figure(figsize=(25, 28), dpi=100, facecolor="white")
+        self.fig.subplots_adjust(left=0.05, right=0.95, top=0.95, bottom=0.175, hspace=0.4, wspace=0.4)
+        self.frame_figure.canvas = FigureCanvasTkAgg(self.fig, master=self.frame_figure)
+        self.frame_figure.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+        ax = self.fig.add_subplot(111)
+        ax.set_title("Stock Price")
+        # ax.set_xlabel("Date")
+        # ax.set_ylabel("Price")
+
+        x = pd.to_datetime(self.data["Date"][self.start_index:self.end_index])
+        y = self.data["Price"][self.start_index:self.end_index]
+        ax.plot(x, y, label="Price", color="blue")
+
+        # Set x-axis limits
+        ax.set_xlim(x.min(), x.max())
+
+        # Rotate x-axis labels
+        ax.tick_params(axis='x', rotation=45)
+
+        # Add grid
+        ax.grid()
+
+        # Add legend
+        ax.legend()
+
+        # Draw the figure
+        self.frame_figure.canvas.draw()
--- main.py
+++ main.py
@@ -1,9 +1,40 @@
 import tkinter as tk
 import pandas as pd
+from Stock_info import StockInfo
+import tkinter.ttk as ttk
+from draw_figure import DrawFigure
 
 if __name__ == "__main__":
     wn = tk.Tk()
     wn.title("Computational Finance")
     wn.geometry("800x600")
     wn.configure(bg="white")
+
+    h, w = wn.winfo_screenheight(), wn.winfo_screenwidth()
+
+    # Create treeview with a scrollbar
+    frame_table = tk.Frame(wn)
+    frame_table.place(relx=0.025, rely=0.05, relwidth=0.3, relheight=0.9, anchor="nw")
+    Stock_columns = ["Name", "Last", "High", "Low", "Chg.", "Chg. %", "Vol.", "Time"]
+    yscrollbar = tk.Scrollbar(frame_table, orient="vertical")
+    tv = ttk.Treeview(master=frame_table, columns=Stock_columns, show="headings", yscrollcommand=yscrollbar.set)
+    tv.pack(fill="both", expand=True)
+    for col in Stock_columns:
+        tv.heading(col, text=col, anchor="center")
+        tv.column(col, anchor="center", width=tv.winfo_width() // len(Stock_columns))
+    yscrollbar.config(command=tv.yview)
+    stki = StockInfo(tv)
+    stki.load_from_csv("Stock Quotes.csv")
+
+    # Create a frame show the figure
+    frame_figure = tk.Frame(wn)
+    frame_figure.place(relx=0.35, rely=0.05, relwidth=0.625, relheight=0.75, anchor="nw")
+    stkf = DrawFigure(frame_figure)
+    stkf.load_data_csv("Boeing")
+    stkf.Draw()
+
+    frame_indicators = tk.Frame(wn)
+    frame_indicators.place(relx=0.35, rely=0.825, relwidth=0.625, relheight=0.125, anchor="nw")
+
+
     wn.mainloop()
